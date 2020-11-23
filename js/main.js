87fbf9164c9ef81192ae8417606c35547e027822
LucasRalf14 <lucas_w.o.ralf@hotmail.com>
2020-11-23 18:51:02 -0300

    Primeiros passos no JavaScript, depois do repositório ser clonado, foram adicionados arquivos javaScript e html com alguns códigos iniciais

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Lucas Ralf";
+var n1 = 23;
+var n2 = 10;
+var frase = "Japão é a melhor seleão do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.toUpperCase());
+//(frase.replace("Japão", "Brasil"));
