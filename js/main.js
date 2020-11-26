ca035f7ded9f33faa165da84a099925685291266
LucasRalf14 <lucas_w.o.ralf@hotmail.com>
2020-11-26 18:48:29 -0300

    Inserido códigos de funções

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
-var d = new Date();
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+function validaIdade(idade){
+    var validar;
+    if(idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+var idade = prompt("Qual a sua idade?");
+console.log(validaIdade(idade))
+
+//alert(soma(5, 10));
+
+/*var d = new Date();
 alert(d.getDay());
 alert(d.getHours());
 alert(d.getMinutes());
+*/
+
 
 /*var count;
 for(count = 0; count <=5; count++){
